596afe73d22f2963c31f81dd4aba9057c1ae0e6f
hlasbeztvare-code <hlasbeztvare@gmail.com>
2026-04-07 13:35:45 +0200

    fix: use user's blackdead.png with transparent background

--- src/components/home/WowHero.tsx
+++ src/components/home/WowHero.tsx
@@ -97,7 +97,7 @@ export default function WowHero() {
               className="drop-shadow-[0_40px_120px_rgba(225,6,0,0.7)]" // Zvýšený, temně rudý stín
             >
               <Image 
-                src="/images/products/deadpump.webp" 
+                src="/images/products/blackdead.png" 
                 alt="Black Dead"
                 width={600}
                 height={800}
